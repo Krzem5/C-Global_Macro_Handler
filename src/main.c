9a303fca8121afc2fdf2a1d9bf15c4bb3850e452
Krzem <krzem5.dev@gmail.com>
2021-07-05 19:42:57 +0200

    Push Update 05/07/2021, 19:42:53

--- src/main.c
+++ src/main.c
@@ -3,15 +3,26 @@
 
 
 
-void func(void){
+#define VK_F2 0x71
+
+
+
+void func1(void){
 	printf("Ctrl+Alt+Shift+T pressed!\n");
+}
+
+
+
+void func2(void){
+	printf("Ctrl+Alt+Shift+F2 pressed!\n");
 }
 
 
 
 int main(int argc,const char** argv){
 	init_handlers();
-	register_handler('T',func);
+	register_handler('T',func1);
+	register_handler(VK_F2,func2);
 	start_handlers();
 	return 0;
 }
